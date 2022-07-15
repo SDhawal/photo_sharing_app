207e71dc0ecac5de09626bd4e9ce99e6e4bd2487
Dhawal <dhawalsharma26@icloud.com>
2022-07-14 20:40:38 -0400

    added circular image upload option in signup page

--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -51,6 +51,25 @@ class _SignupScreen extends State<SignupScreen> {
               const SizedBox(
                 height: 64,
               ),
+              // a circular widget to add
+              Stack(
+                children: [
+                 const CircleAvatar(
+                    radius: 64,
+                    backgroundImage: NetworkImage(
+                        'https://images.unsplash.com/photo-1657653464532-3e3015daa4a7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'),
+                  ),
+                  Positioned(
+                    bottom: -10,
+                      left: 80,
+                      child: IconButton(
+                          onPressed: () {},
+                          icon: const Icon(Icons.add_a_photo)))
+                ],
+              ),
+              const SizedBox(
+                height: 24,
+              ),
               // textfield for username
               TextFieldInput(
                   textInputType: TextInputType.text,
